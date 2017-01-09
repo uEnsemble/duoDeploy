3f107fc3f4eaa50980c7f4aef30f7d105799038a
Jesse C. Antoszyk <jcantosz@us.ibm.com>
2017-01-09 12:41:36 -0500

    Execute from current directory

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-
-BIN_DIR="$(dirname $0)"
-
-cd tmp
-ls -l
-BIN_DIR="../$BIN_DIR"
-
 echo "Deployment to bluemix started"
 
-ansible-playbook ../playbook.yml
+ansible-playbook playbook.yml
 rc=$?
 
 #Don't exit != 0 or we cannot create new deploy events
